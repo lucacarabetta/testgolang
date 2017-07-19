9c0e5a5151536ef2d668013f4eb1d843c2dbd96e
luca <luca.carabetta@gmail.com>
2017-07-19 16:40:58 +0200

    test

--- app.go
+++ app.go
@@ -1,18 +1,24 @@
 package main
 
 import	(
-			/*"fmt"*/
+			"fmt"
 			"net/http"
 			"log"
+			"os"
 		)
 
 func httpRequests(w http.ResponseWriter, r *http.Request)	{
-	/*r.ParseForm()
+	
+	r.ParseForm()
 	fmt.Fprintf(w, r.URL.Path)
+
 	switch r.URL.Path	{
-	case "/send/ether":
+		case "/exit":
+			os.Exit(0)
+		case "/send/ether":
+			fmt.Fprintf(w, "Sending Ether from"+ eth.coinbase +"to"+ eth.accounts[1])
 			eth.sendTransaction({from:eth.coinbase, to:eth.accounts[1], value: web3.toWei(0.05, "ether")})
-	}*/
+	}
 }
 
 func connectToBlockchain()	{
@@ -28,7 +34,7 @@ func sendValue()	{
 
 func main()	{
 	http.HandleFunc("/", httpRequests)
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":9990", nil)
 	if err != nil	{
 		log.Fatal("ListenAndServe: ", err)
 	}
