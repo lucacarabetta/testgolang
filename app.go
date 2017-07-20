38ccb37e6dc3114c27bab89c3350e6ff6e98a4af
luca <luca.carabetta@gmail.com>
2017-07-20 09:34:15 +0200

    test

--- app.go
+++ app.go
@@ -11,14 +11,14 @@ import	(
 func httpRequests(w http.ResponseWriter, r *http.Request)	{
 	
 	r.ParseForm()
-	fmt.Fprintf(w, r.URL.Path)
+	fmt.Fprintf(w, r.URL.Path +"<br>")
 
 	switch r.URL.Path	{
 		case "/exit":
 			os.Exit(0)
 		case "/send/ether":
 			fmt.Fprintf(w, "Sending Ether")
-			//fmt.Fprintf(w, "Sending Ether from"+ eth.coinbase +"to"+ eth.accounts[1])
+			fmt.Fprintf(w, "Sending Ether from"+ eth.coinbase +"to"+ eth.accounts[1])
 			//eth.sendTransaction({from:eth.coinbase, to:eth.accounts[1], value: web3.toWei(0.05, "ether")})
 	}
 }
